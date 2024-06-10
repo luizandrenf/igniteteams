24aa216bcd2fe156aeda78695540be8894972700
luizandrenf <luizandrenf@gmail.com>
2024-06-10 16:54:38 -0400

    create of ButtonIcon component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,9 @@
 import { Header } from "@components/Header";
-import { Container } from "./styles";
+import { ButtonIcon } from "@components/ButtonIcon";
 import { Highlight } from "@components/Highlight";
+import { Input } from "@components/Input";
+
+import { Container, Form } from "./styles";
 
 export function Players(){
 
@@ -12,6 +15,16 @@ export function Players(){
         title="Nome da turma"
         subtitle="adicionar a galera e separe os times"
       />
+
+      <Form>
+        <Input
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+        />
+        <ButtonIcon 
+          icon="add"
+        />
+      </Form>
     </Container>
   );
 }
