7b615c49cd977337207f48074d1b66e6a1875e55
luizandrenf <luizandrenf@gmail.com>
2024-06-10 10:32:20 -0400

    first commit

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,8 @@
+import 'styled-components/native';
+import  { defaultTheme}  from '../theme';
+
+declare module 'styled-components/native' {
+  type ThemeType = typeof defaultTheme;
+
+  export interface DefaultTheme extends ThemeType{}
+}
